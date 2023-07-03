fc1ab2dc55e03202fb4f37323ec95a8ce2171ccf
Michael <gadreelazazel@gmail.com>
2023-07-03 14:39:17 +0100

    Deployed to mumbai testnet

---
+++ AuditProject/scripts/contractAddress.js
@@ -0,0 +1,3 @@
+
+    export const contractAddress = "0x6969cC92F2f0028eA8746d3D64fCD64f42AcE834"
+  
---
+++ InsuranceProject/scripts/contractAddress.js
@@ -0,0 +1,3 @@
+
+    export const factoryAddress = "0x90040E4915D2b217ab5691C950A64a672217C0c3"
+  
